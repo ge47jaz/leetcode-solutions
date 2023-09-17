67f19f07abb023df70a550a703264928176abe2f
Christian Horne <christianhorne30@yahoo.com>
2023-09-17 19:27:59 +0200

    added first leetcode exercise, two-sum

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -5,14 +5,34 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = nums.length;
+
+        //construct hashmap
+        for (int i = 0; i < n; i++){
+            map.put(nums[i], i);
+        }    
        
+        //find complement
+        for (int i = 0; i < n; i++){
+            int comp = target - nums[i];
+            if (map.containsKey(comp) && map.get(comp) != i){
+                return new int[]{i, map.get(comp)};
+            }
+        }
        
-       
-       
-        return null; 
+        //no result found
+        return new int[]{};
     }
 }
+
+public static main
 // @lc code=end
 
