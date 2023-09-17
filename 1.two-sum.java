ec083db623fc4117189c07740d82f90313517e88
Christian Horne <christianhorne30@yahoo.com>
2023-09-17 16:14:43 +0200

    first commit

---
+++ 1.two-sum.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       
+       
+       
+       
+        return null; 
+    }
+}
+// @lc code=end
+
