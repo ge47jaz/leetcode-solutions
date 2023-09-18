1b8afce0c0974d8ed2f4e0aaaef0dced69927b33
ge47jaz <ge47jaz@mytum.de>
2023-09-18 11:50:26 +0200

    added 9, 13, 14, 29, 21, 26

---
+++ 9.palindrome-number.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int y = 0, z = x;
+        while (z != 0) {
+            y = y * 10 + z % 10;
+            z /= 10;
+        }
+        return x == y;
+    }
+
+}
+// @lc code=end
+
